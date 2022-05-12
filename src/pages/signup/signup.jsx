9f22160960e1a82b8e8ff15c026ca3db7294a59a
Karol Sontak <karolsontak@gmail.com>
2022-05-12 15:13:48 -0300

    Login e Cadastro

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -1,30 +1,28 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import useFormSignup from './useSignupForm';
 
 const SignUp = () => {
-  const history = useNavigate();
+  const { handleChange, handleSubmit } = useFormSignup();
 
   return (
     <div className='main'>
       <section className='signup-page'>
         <h1 className='signup-title'>Cadastre-se</h1>
         <label className='signup-labels'>Seu nome</label>
-        <input className='signup-input' type='text' placeholder='Nome' name='name' autoComplete='off'/>
+        <input className='signup-input' type='text' placeholder='Nome' name='name' autoComplete='off' onChange={handleChange}/>
         <label className='signup-labels'>Cargo</label>
-        <select className='signup-select' autoComplete='off' name=''>
+        <select className='signup-select' autoComplete='off' name='role' onChange={handleChange}>
           <option value=''>Selecione um cargo</option>
           <option value='attendant'>Atendente</option>
           <option value='chef'>Chef de Cozinha</option>
         </select>
         <label className='signup-labels'>Email</label>
-        <input className='signup-input' type='email' placeholder='username@example.com' name='email' autoComplete='off' />
+        <input className='signup-input' type='email' placeholder='username@example.com' name='email' autoComplete='off' onChange={handleChange}/>
         <label className='signup-labels'>Senha</label>
-        <input className='signup-input' type='password' placeholder='Senha' name='password' />
-        <label className='signup-labels'>Confirme sua senha</label>
-        <input className='signup-input' type='password' placeholder='Confirme sua senha' name='confirmPassword' />
+        <input className='signup-input' type='password' placeholder='Senha' name='password' onChange={handleChange} />
         <span className='errors-message'>
         </span>
-        <button className='signup-button draw' type='submit' onClick={() => { history('/login') }}>Cadastrar</button>  
+        <button className='signup-button draw' type='submit' onClick={handleSubmit}>Cadastrar</button>  
       </section>
     </div>
   );
